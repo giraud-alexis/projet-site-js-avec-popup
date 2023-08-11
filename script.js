67ba3539efa5542877a940db78851bdf070f0462
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-11 10:34:10 +0200

    codage plus propre + ajout de suppression de popup

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-const nav = document.querySelector("#navbar");
-const improvise = document.querySelector("#imgImprovise");
-const popup = document.querySelector("#popup");
-const clic = document.querySelector("#closeBtn");
+//const nav = document.querySelector("#navbar");
+//const improvise = document.querySelector("#imgImprovise");
+//const popup = document.querySelector("#popup");
+//const clic = document.querySelector("#closeBtn");
 
+//window.addEventListener("scroll", () => {
+//  if (window.scrollY > 40) {
+//    nav.style.height = "50px";
+//  } else {
+//    nav.style.height = "90px";
+//  }
+//});
+
+//window.addEventListener("scroll", () => {
+//  if (window.scrollY > 370) {
+//    improvise.style.opacity = 1;
+//    improvise.style.transform = "none";
+//  } else {
+//    improvise.style.opacity = 0;
+//    improvise.style.transform = "else";
+//  }
+//});
+
+//window.addEventListener("scroll", () => {
+//  if (window.scrollY > 1350) {
+//    popup.style.opacity = 1;
+//    popup.style.transform = "none";
+//  }
+//});
+
+//clic.addEventListener("click", () => {
+//  popup.style.opacity = 0;
+//});
+//-------------------------------------------------------------------------
+//connaitre le pourcentage de scroll
+//console.log([window.scrollY + window.innerHeight] / document.body.offsetHeight);
+//-------------------------------------------------------------------------
+
+//solution secondaire et plus propre de faire de faire -----------
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 40) {
-    nav.style.height = "50px";
+  if (window.scrollY > 50) {
+    navbar.style.height = "50px";
   } else {
-    nav.style.height = "90px";
+    navbar.style.height = "90px";
   }
-});
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+  //console.log(scrollValue);
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 370) {
-    improvise.style.opacity = 1;
-    improvise.style.transform = "none";
-  } else {
-    improvise.style.opacity = 0;
-    improvise.style.transform = "else";
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
   }
-});
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 1350) {
+  if (scrollValue > 0.85 && playone) {
     popup.style.opacity = 1;
     popup.style.transform = "none";
+    playone = false;
   }
 });
 
-clic.addEventListener("click", () => {
+closeBtn.addEventListener("click", () => {
   popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)";
 });
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+//-------------------------------------------------------------
+//faire en sorte que la popup ne revienne jamais
+let playone = true;
+//suite de cette fonction dans if/scrollValue/popup
